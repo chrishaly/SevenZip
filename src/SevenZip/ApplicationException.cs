adccbf02226abd6f301c06a5268b8828d52d90cd
Chris <chrishwang@live.cn>
2016-12-31 12:21:42 +0800

    init

---
+++ src/SevenZip/ApplicationException.cs
@@ -0,0 +1,19 @@
+﻿#if NETCORE
+
+// ReSharper disable once CheckNamespace
+namespace System
+{
+	public class ApplicationException : Exception
+	{
+		public ApplicationException()
+		{
+		}
+
+		public ApplicationException(string message)
+			: base(message)
+		{
+		}
+	}
+}
+
+#endif
